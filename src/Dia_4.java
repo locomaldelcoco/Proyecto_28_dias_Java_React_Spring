8297e8505d6d9c494e91dcaf21710c6eef4a0cb2
locomaldelcoco <santiicarp1@gmail.com>
2025-08-04 14:12:50 -0300

    Creo 2 personas para verificar constructores y metodos

--- src/Dia_4.java
+++ src/Dia_4.java
@@ -1,5 +1,9 @@
 public class Dia_4 {
     public static void main(String[] args) {
-        
+        Persona persona1 = new Persona("Santiago",25,42545214);
+        Persona persona2 = new Persona();
+
+        persona1.mostrarInformacion();
+        System.out.println(persona1.esMayorDeEdad());
     }
 }
