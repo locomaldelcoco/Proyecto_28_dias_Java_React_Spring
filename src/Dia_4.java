f534d224e8dffe80b635c73c785c6d83a50a7f1e
locomaldelcoco <santiicarp1@gmail.com>
2025-08-04 20:30:31 -0300

    Se agrega Scanner en Dia_4

--- src/Dia_4.java
+++ src/Dia_4.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Dia_4 {
     public static void main(String[] args) {
+        String nombre; int edad, dni;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese el nombre de la persona: ");nombre = input.next();
+        System.out.println("Ingrese la edad de la persona: ");edad = input.nextInt();
+        System.out.println("Ingrese el dni de la persona: ");dni = input.nextInt();
+
         Persona persona1 = new Persona("Santiago",25,42545214);
-        Persona persona2 = new Persona();
+        Persona persona2 = new Persona(nombre,edad,dni);
 
         persona1.mostrarInformacion();
         System.out.println("Es mayor de edad: " + persona1.esMayorDeEdad());
