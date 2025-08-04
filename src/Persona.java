21b368626845300454d0de8be1b468e6e1592191
locomaldelcoco <santiicarp1@gmail.com>
2025-08-04 14:12:27 -0300

    Agrego constructor de Persona con ingreso por consola

--- src/Persona.java
+++ src/Persona.java
@@ -6,12 +6,16 @@ public class Persona {
     int dni;
     Scanner input = new Scanner(System.in);
 
-
-
-    Persona(String nombre, int edad, int dni){
+    Persona(){
         this.nombre = input.next();
         this.edad = input.nextInt();
         this.dni = input.nextInt();
+    }
+
+    Persona(String nombre, int edad, int dni){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
     }
 
     public void mostrarInformacion(){
