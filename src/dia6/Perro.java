83852ca5bc1b93b11e78722c5f350f0c81aca1c7
locomaldelcoco <santiicarp1@gmail.com>
2025-08-07 17:23:06 -0300

    Generé los métodos hacerSonido y mostrarDatos

--- src/dia6/Perro.java
+++ src/dia6/Perro.java
@@ -1,9 +1,19 @@
 package dia6;
 
-public class Perro extends Animal{
+public class Perro extends Animal {
 
-    public Perro (String nombre, int edad){
+    public Perro (String nombre, int edad) {
         super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println(getClass().getSimpleName() + ": " + "Guau");
+    }
+
+    @Override
+    public void mostrarDatos() {
+        super.mostrarDatos();
     }
 
 }
