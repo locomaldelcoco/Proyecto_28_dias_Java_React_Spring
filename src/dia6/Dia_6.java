744e53a04fcdac3a1ed76bb6367073ab698007d8
locomaldelcoco <santiicarp1@gmail.com>
2025-08-07 18:15:10 -0300

    Generé un ArrayList de animales para agregarlos y luego presentarlos en un bucle

--- src/dia6/Dia_6.java
+++ src/dia6/Dia_6.java
@@ -1,18 +1,26 @@
 package dia6;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Dia_6 {
     public static void main(String[] args) {
         Animal perro1 = new Perro("Laser",2);
         Animal gato1 = new Gato("Elvira",4);
         Animal mascota = new Animal("Troy", 2);
+        ArrayList<Animal> animales = new ArrayList<>();
+        animales.add(perro1);
+        animales.add(gato1);
+        animales.add(mascota);
 
-        perro1.mostrarDatos();
-        perro1.hacerSonido();
+        presentarAnimales(animales);
 
-        gato1.mostrarDatos();
-        gato1.hacerSonido();
+    }
 
-        mascota.mostrarDatos();
-        mascota.hacerSonido();
+    private static void presentarAnimales(ArrayList<Animal> animales) {
+        for (Animal animal : animales) {
+            animal.mostrarDatos();
+            animal.hacerSonido();
+        }
     }
 }
