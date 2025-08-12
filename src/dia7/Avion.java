86b096da39099a44b3e45dcd2251e0a5f1887d4b
locomaldelcoco <santiicarp1@gmail.com>
2025-08-11 22:34:44 -0300

    Agrego constructor en Avion e inicio con mostrarDatos

--- src/dia7/Avion.java
+++ src/dia7/Avion.java
@@ -1,9 +1,20 @@
 package dia7;
 
 public class Avion implements Volador{
+    private String nombre;
+    private String tipo;
+
+    public Avion (String nombre, String tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
 
     @Override
     public void volar () {
         System.out.println("Vuelo como un avion");
+    }
+
+    public void mostrarDatos () {
+        System.out.println();
     }
 }
