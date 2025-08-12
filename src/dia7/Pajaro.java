c39db765694e69f320ebb0c411856466ba74d537
locomaldelcoco <santiicarp1@gmail.com>
2025-08-11 22:30:42 -0300

    Agrego constructor en Pajaro e inicio con mostrarDatos

--- src/dia7/Pajaro.java
+++ src/dia7/Pajaro.java
@@ -1,9 +1,20 @@
 package dia7;
 
 public class Pajaro implements Volador{
+    private String nombre;
+    private String tipo;
+
+    public Pajaro(String nombre, String tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
 
     @Override
     public void volar() {
         System.out.println("Nado como un pájaro");
+    }
+
+    public void mostrarDatos () {
+        System.out.println();
     }
 }
