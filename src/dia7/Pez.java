1c5e20128846abd355d3595b6217e7e24ff8a337
locomaldelcoco <santiicarp1@gmail.com>
2025-08-11 22:39:43 -0300

    Agrego constructor en Pez e inicio con mostrarDatos

--- src/dia7/Pez.java
+++ src/dia7/Pez.java
@@ -1,9 +1,20 @@
 package dia7;
 
 public class Pez implements Nadador{
+    private String nombre;
+    private String tipo;
+
+    public Pez (String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
 
     @Override
     public void nadar () {
         System.out.println("Nado como un pez");
+    }
+
+    public void mostrarDatos(){
+        System.out.println();
     }
 }
