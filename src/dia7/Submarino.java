094ae3bb9086c6b45590c68af1b9b640f72762c0
locomaldelcoco <santiicarp1@gmail.com>
2025-08-11 22:38:07 -0300

    Agrego constructor en Submarino e inicio con mostrarDatos

--- src/dia7/Submarino.java
+++ src/dia7/Submarino.java
@@ -1,9 +1,20 @@
 package dia7;
 
 public class Submarino implements Nadador{
+    private String nombre;
+    private String tipo;
+
+    public Submarino (String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
 
     @Override
     public void nadar () {
         System.out.println("Nado pero como un submarino");
+    }
+
+    public void mostrarDatos (){
+        System.out.println();
     }
 }
